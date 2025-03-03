81b48ced37c3d9be60edcc821a6460a47122a13f
thenurim-ceo <ceo@thenurim.life>
2025-03-04 02:23:14 +0900

    fix: add SecureHeaders

--- plugin.rb
+++ plugin.rb
@@ -7,5 +7,3 @@ Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Option
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '*'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
-
-Rails.logger.info "Novalink Feedback plugin initialized successfully"
